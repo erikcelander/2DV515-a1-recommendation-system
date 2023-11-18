af38571cd7fb431c8987daa33c8f77545f7d3f28
Erik Kroon Celander <ek223ur@student.lnu.se>
2023-11-18 19:04:53 +0100

    add variables for select values

--- components/select-card.tsx
+++ components/select-card.tsx
@@ -1,6 +1,8 @@
-import * as React from "react"
+"use client";
+import * as React from "react";
+import { useState } from 'react';
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -8,26 +10,41 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
+} from "@/components/ui/select";
 
-import{ User, Movie } from "@/types/types";
+import { User, Movie } from "@/types/types";
 
-
-export async function SelectCard({ users, movies }: { users: User[], movies: Movie[] }) {
+export function SelectCard({ users, movies }: { users: User[], movies: Movie[] }) {
+  const [selectedName, setSelectedName] = useState('');
+  const [selectedMovie, setSelectedMovie] = useState('');
+  const [selectedResults, setSelectedResults] = useState('');
 
   const resultsOptions = Array.from({ length: 10 }, (_, i) => i + 1);
 
+  const handleFindUsersClick = () => {
+    console.log({
+      name: selectedName,
+      movie: selectedMovie,
+      results: selectedResults,
+    });
+  };
 
-  
+  const handleFindMoviesClick = () => {
+    console.log({
+      name: selectedName,
+      movie: selectedMovie,
+      results: selectedResults,
+    });
+  };
+
   return (
     <Card className="w-[450px]">
       <CardHeader>
@@ -35,54 +52,56 @@ export async function SelectCard({ users, movies }: { users: User[], movies: Mov
         <CardDescription>Find recommended movies and similar users.</CardDescription>
       </CardHeader>
       <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Select>
-                <SelectTrigger id="name">
-                  <SelectValue placeholder="Select Name" />
-                </SelectTrigger>
-                <SelectContent position="popper">
-                  {users.map((user) => (
-                    <SelectItem key={user.UserId} value={user.Name}>{user.Name}</SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="movie">Movie</Label>
-              <Select>
-                <SelectTrigger id="movie">
-                  <SelectValue placeholder="Select Movie" />
-                </SelectTrigger>
-                <SelectContent position="popper">
-                  {movies.map((movie) => (
-                    <SelectItem key={movie.MovieId} value={movie.Title}>{movie.Title}</SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="results">Results</Label>
-              <Select>
-                <SelectTrigger id="results">
-                  <SelectValue placeholder="Select amount of results" />
-                </SelectTrigger>
-                <SelectContent position="popper">
-                  {resultsOptions.map((number) => (
-                    <SelectItem key={number} value={String(number)}>{number}</SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            </div>
+        <div className="grid w-full items-center gap-4">
+          <div className="flex flex-col space-y-1.5">
+            <Label htmlFor="name">Name</Label>
+            <Select onValueChange={setSelectedName}>
+              <SelectTrigger id="name">
+                <SelectValue placeholder="Select Name" />
+              </SelectTrigger>
+              <SelectContent position="popper">
+                {users.map((user) => (
+                  <SelectItem key={user.UserId} value={user.Name}>{user.Name}</SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
           </div>
-        </form>
+          <div className="flex flex-col space-y-1.5">
+            <Label htmlFor="movie">Movie</Label>
+            <Select onValueChange={setSelectedMovie}>
+              <SelectTrigger id="movie">
+                <SelectValue placeholder="Select Movie" />
+              </SelectTrigger>
+              <SelectContent position="popper">
+                {movies.map((movie) => (
+                  <SelectItem key={movie.MovieId} value={movie.Title}>{movie.Title}</SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="flex flex-col space-y-1.5">
+            <Label htmlFor="results">Results</Label>
+            <Select onValueChange={setSelectedResults}>
+              <SelectTrigger id="results">
+                <SelectValue placeholder="Select amount of results" />
+              </SelectTrigger>
+              <SelectContent position="popper">
+                {resultsOptions.map((number) => (
+                  <SelectItem key={number} value={String(number)}>{number}</SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
       </CardContent>
       <CardFooter className="flex justify-around">
-        <Button variant="outline">Find top matching users</Button>
-        <Button>Find recommended movies</Button>
+        <Button variant="outline" onClick={handleFindUsersClick}>
+          Find top matching users
+        </Button>
+        <Button onClick={handleFindMoviesClick}>
+          Find recommended movies
+        </Button>
       </CardFooter>
     </Card>
-  )
+  );
 }
